c51a130e9ff1c9b3daca6a1bc35232e1e5c4558e
EneroMangal4321 <enero.mangal@student.rocva.nl>
2020-03-25 15:59:43 +0100

    Update test-rule.py

--- tips/tests/test-rule.py
+++ tips/tests/test-rule.py
@@ -1,14 +1,16 @@
 from objectpath import Tree
+import os
 
 inputData = {
-    "info": "2020-09-30",
-    "kinderen" : {
-        "geboortedatum": "2019-01-01"
-    }
+    "info": "2020-09-30"
+    # "kinderen" : {
+    #     "geboortedatum": "2019-01-01"
+    # }
 }
 
+BRP = os.path.join(PROJECT_PATH, 'tests', 'fixtures', 'data', 'brp.json')
 
 treedata = Tree(inputData)
 
-rule = 'dateTime($.kinderen.geboortedatum) + timeDelta(18, 0, 0, 0, 0, 0) > dateTime($.info)'
+rule = 'dateTime($.brp.kinderen.geboortedatum) + timeDelta(18, 0, 0, 0, 0, 0) > dateTime($.info)'
 print("WAT IS DIT", treedata.execute(rule))
