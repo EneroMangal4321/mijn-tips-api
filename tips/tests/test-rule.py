bd62c71452d8099a0a6815ed5c96dfc3f676024b
Enero <2082263@talnet.nl>
2020-04-22 17:16:41 +0200

    Update test-rule.py

--- tips/tests/test-rule.py
+++ tips/tests/test-rule.py
@@ -2,23 +2,23 @@ from objectpath import Tree
 from unittest import TestCase
 
 
-inputData = {
-    "info": "2020-09-30",
-    "kinderen": [
-        {
-            "geboortedatum": "1995-01-01"
-        },
-        {
-            "geboortedatum": "2005-01-01"
-        }
-    ]
-}
-rule = 'dateTime($.kinderen.geboortedatum[1]) + timeDelta(18, 0, 0, 0, 0, 0) > dateTime($.info)'
-treedata = Tree(inputData)
-for geboortedatum in inputData:
-    print(geboortedatum)
 
-print("WAT IS DIT", treedata.execute(rule))
 
 def test_rule():
-    
+    inputData = {
+        "info": "2020-09-30",
+        "kinderen": [
+            {
+                "geboortedatum": "1995-01-01"
+            },
+            {
+                "geboortedatum": "2005-01-01"
+            }
+        ]
+    }
+    rule = 'dateTime($.kinderen.geboortedatum[1]) + timeDelta(18, 0, 0, 0, 0, 0) > dateTime($.info)'
+    treedata = Tree(inputData)
+    for geboortedatum in inputData:
+        print(geboortedatum)
+
+    print("WAT IS DIT", treedata.execute(rule))
