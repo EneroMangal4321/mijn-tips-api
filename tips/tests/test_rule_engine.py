8d0de2cbdc951b388a6b16807b5e7481a8b28fb9
Enero <2082263@talnet.nl>
2020-05-06 14:33:14 +0200

    Update test_rule_engine.py

--- tips/tests/test_rule_engine.py
+++ tips/tests/test_rule_engine.py
@@ -4,7 +4,7 @@ import objectpath
 
 from tips.generator.rule_engine import apply_rules
 
-from fixtures.fixture import get_fixture
+from tips.tests.fixtures.fixture import get_fixture
 
 
 def get_fixture_stadspas():
