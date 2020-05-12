fc20103be2488be15da1d136eff3652191d80e8d
Enero <2082263@talnet.nl>
2020-05-12 14:56:27 +0200

    Update test_rule_engine.py

--- tips/tests/test_rule_engine.py
+++ tips/tests/test_rule_engine.py
@@ -7,43 +7,6 @@ from tips.generator.rule_engine import apply_rules
 from tips.tests.fixtures.fixture import get_fixture
 
 
-tips_pool = [
-    {
-        "id": "mijn-1",
-        "active": True,
-        "priority": 50,
-        "datePublished": "2019-07-24",
-        "title": "Geen telefoon op de fiets",
-        "description": "U mag geen telefoon meer vasthouden op de fiets",
-        "link": {
-            "title": "Meer informatie",
-            "to": "https://www.rijksoverheid.nl/onderwerpen/fiets/vraag-en-antwoord/mag-ik-bellen-en-naar-muziek-luisteren-op-de-fiets"
-        },
-        "imgUrl": "api/tips/static/tip_images/bellenopfiets.jpg"
-
-    },
-    {
-        "id": "mijn-11",
-        "active": True,
-        "priority": 70,
-        "datePublished": "2019-10-22",
-        "title": "Wat kan ik doen met mijn Stadspas?",
-        "description": "U hebt een Stadspas",
-        "rules": [
-            {"type": "ref", "ref_id": "3"},
-            {"type": "ref", "ref_id": "1"},
-        ],
-        "isPersonalized": True,
-        "link": {
-            "title": "Bekijk de aanbiedingen",
-            "to": "https://www.amsterdam.nl/toerisme-vrije-tijd/stadspas/"
-        },
-        "imgUrl": "/api/tips/static/tip_images/stadspas.jpg"
-    }
-]
-
-
-
 class RuleEngineTest(TestCase):
     def setUp(self) -> None:
         _test_data = {
